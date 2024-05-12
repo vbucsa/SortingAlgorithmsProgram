b910abfcef02f95b64f7b240c19b430976e925fb
vbucsa <62548136+vbucsa@users.noreply.github.com>
2024-05-12 22:34:28 +0300

    Add files via upload

---
+++ mpi.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1; 
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high); 
+
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        }
+        else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) {
+        arr[k++] = L[i++];
+    }
+    while (j < n2) {
+        arr[k++] = R[j++];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            std::swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+int main() {
+    const int SIZE = 1000;
+    int* arr = new int[SIZE];
+
+    srand(time(NULL));
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 1000000; // Range: 0 to 999999
+    }
+
+    clock_t start = clock();
+    bubbleSort(arr, SIZE);
+    clock_t end = clock();
+
+    double timeTaken = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Time taken by Insertion Sort: " << timeTaken << " seconds\n";
+
+    delete[] arr;
+
+    return 0;
+}
